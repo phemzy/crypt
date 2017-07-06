f4b824563ac46bb03ffd482d784278994e7b1f3c
= <=>
2017-07-07 00:06:26 +0100

    Add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,12 @@ class AppServiceProvider extends ServiceProvider
         \View::share('markets', \App\Market::all());
 		\View::share('packages', \App\Package::all());
 		Schema::defaultStringLength(191);
+
+        \Cloudinary::config(array( 
+          "cloud_name" => "crypto2naira", 
+          "api_key" => "328753983467173", 
+          "api_secret" => "hzqND4v2tKPkXs-r8mB-siWQ6lA" 
+        ));
     }
 
     /**

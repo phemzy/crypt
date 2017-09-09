523af19dd04749339124f20c2b48426b46628eca
= <=>
2017-09-09 14:24:41 +0100

    Added

--- resources/views/partials/item.blade.php
+++ resources/views/partials/item.blade.php
@@ -5,7 +5,7 @@
         <p class="font-w600"> {{ $t->type =='purchase' ? $t->market->abbr_name . ' Purchased' : $t->market->abbr_name . ' Sold' }} <a href="{{ route('transaction.single', $t->id) }}" class="btn btn-primary btn-xs"> View </a></p>
         <p class="font-s13">Package:  <b>{{ $t->package->name }}</b></p>
         <p class="font-s13">Status:  <b class="text-capitalize">{{ $t->status }}</b></p>
-        @if($t->status == 'pending' && $t->type == 'sell' && $t->market->abbr_name == 'GRC')
+        @if($t->status == 'pending' && $t->market->abbr_name == 'GRC')
 			<p>
 				<a href="{{ route('convert_to_tbc', $t->id) }}" class="btn btn-danger btn-xs">Move to TBC</a>
 			</p>
